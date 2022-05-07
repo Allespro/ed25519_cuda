86e98b899958d8206bd3f4c4ddbda49813518cb3
Allespro <arcy.all@yahoo.com>
2022-05-07 21:51:44 +0300

    keccak

--- onion.cu
+++ onion.cu
@@ -1,23 +1,19 @@
 #include "keccakf.cuh"
 
-__global__ void hash(uint8_t *checksum) {
-  // pad checksum up r bits
-  checksum[48] = 0x01; // pad10*1 start
-  checksum[135] = 0x80; // pad10*1 end
-
-  // cast checksum to S for keccakf implementation (blocks of 64 bits)
-  uint64_t *S[25];
-  for (size_t i = 0; i < 25; i++) {
-    S[i] = (uint64_t *)&checksum[i * 8];
-  }
-
-  // run the keccakf transformation
-  keccakf<<<1,1>>>(*S); 
-
-}
 
 __host__ void onion_address(unsigned char *public_key, uint8_t *checksum) {
     for (int i = 0; i < 32; ++i) checksum[15 + i] = public_key[i];
     checksum[47] = 0x03;
-    hash(checksum);
+    // pad checksum up r bits
+    checksum[48] = 0x01; // pad10*1 start
+    checksum[135] = 0x80; // pad10*1 end
+
+    // cast checksum to S for keccakf implementation (blocks of 64 bits)
+    uint64_t *S[25];
+    for (size_t i = 0; i < 25; i++) {
+        S[i] = (uint64_t *)&checksum[i * 8];
+    }
+
+    // run the keccakf transformation
+    keccakf<<<1,1>>>(*S); 
 }
